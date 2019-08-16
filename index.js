5c4c2303a5fa0440cf027828248da59840751a96
Sean Glancy <spglancy@gmail.com>
2019-08-15 17:09:50 -0700

    added forecasts

--- index.js
+++ index.js
@@ -8,5 +8,14 @@
             })
     }
 
+    const simpleForecast = (zip) => {
+        return axios.post(`https://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&appid=36697159ee2bad94bfb8136b848fdb6b`)
+            .then(res => {
+                return res.data.list.map(obj => {
+                    return [parseInt(((obj.main.temp - 273) * 9 / 5) + 32), obj.weather[0].main, obj.dt_txt]
+                })
+            })
+    }
     simpleWeather(94102).then(res => console.log(res))
+    simpleForecast(94102).then(res => console.log(res))
 })()
